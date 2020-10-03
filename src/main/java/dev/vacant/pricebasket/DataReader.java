f2cf71a8c260f653a0148e579e71188da91d3235
doedrim <git@doedrim.net>
2020-10-04 00:58:16 +0100

    Add data file reader utility

---
+++ src/main/java/dev/vacant/pricebasket/DataReader.java
@@ -0,0 +1,64 @@
+package dev.vacant.pricebasket;
+
+import java.io.*;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Utility for reading data files.
+ * <p>
+ * Default data files are packaged with the application, however the user can
+ * override this by providing its own data files in the file system (relative to
+ * the working directory). This class ensures a proper handling of the data file
+ * location discovery.
+ */
+public class DataReader {
+    private final FileSystem fileSystem;
+
+    /**
+     * Creates a new data file reader backed by the file system provided.
+     *
+     * @param fileSystem The file system to use for locating data files.
+     */
+    public DataReader(FileSystem fileSystem) {
+        this.fileSystem = requireNonNull(fileSystem, "fileSystem is required");
+    }
+
+    /**
+     * Creates a new data file reader backed by the default file system.
+     */
+    public DataReader() {
+        this(FileSystems.getDefault());
+    }
+
+    /**
+     * Creates a new reader (with line number support) for a data file.
+     * <p>
+     * This method will return a reader for the data file with the most
+     * precedence that currently exists. This means that if a data file exists
+     * in the backing file system then that one will be returned instead of the
+     * default one packaged with the application.
+     *
+     * @param filePath The relative path to the data file
+     * @return The line number reader for the data file.
+     * @throws IOException If an I/O error occurs.
+     */
+    public LineNumberReader newLineNumberReader(String filePath) throws IOException {
+        InputStream stream = newInputStream(filePath);
+        return new LineNumberReader(new InputStreamReader(stream, UTF_8));
+    }
+
+    private InputStream newInputStream(String filePath) throws IOException {
+        Path path = fileSystem.getPath(filePath);
+        if (Files.exists(path))
+            return Files.newInputStream(path);
+        InputStream stream = getClass().getClassLoader().getResourceAsStream(filePath);
+        assert stream != null;
+        return stream;
+    }
+}
---
+++ src/test/java/dev/vacant/pricebasket/DataReaderTest.java
@@ -0,0 +1,73 @@
+package dev.vacant.pricebasket;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.nio.file.FileSystem;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.spi.FileSystemProvider;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@SuppressWarnings("unchecked")
+@DisplayName("DataReader Unit Tests")
+class DataReaderTest {
+    FileSystemProvider provider;
+    FileSystem fileSystem;
+    Path path;
+
+    LineNumberReader reader;
+
+    @BeforeEach
+    void setupMockedPath() {
+        provider = mock(FileSystemProvider.class);
+        fileSystem = mock(FileSystem.class);
+        path = mock(Path.class);
+
+        when(fileSystem.getPath(anyString())).thenReturn(path);
+        when(fileSystem.provider()).thenReturn(provider);
+        when(path.getFileSystem()).thenReturn(fileSystem);
+    }
+
+    @AfterEach
+    void cleanupInputStream() throws IOException {
+        if (reader != null)
+            reader.close();
+    }
+
+    @Test
+    @DisplayName("Read file from file system when path exists")
+    void testExistingPath() throws IOException {
+        byte[] bytes = "File System Contents".getBytes(UTF_8);
+        final InputStream stream = new ByteArrayInputStream(bytes);
+        when(provider.newInputStream(path)).thenReturn(stream);
+
+        assertTrue(Files.exists(path), "Failed to setup existing path");
+
+        final DataReader dataReader = new DataReader(fileSystem);
+        reader = dataReader.newLineNumberReader("testfiles/datafile");
+
+        assertEquals("File System Contents", reader.readLine());
+    }
+
+    @Test
+    @DisplayName("Read included file when path doesn't exist")
+    void testNonExistingPath() throws IOException {
+        doThrow(IOException.class).when(provider).readAttributes(eq(path), any(Class.class));
+        doThrow(IOException.class).when(provider).readAttributes(eq(path), anyString());
+        doThrow(IOException.class).when(provider).checkAccess(path);
+
+        assertFalse(Files.exists(path), "Failed to setup non-existing path");
+
+        final DataReader dataReader = new DataReader(fileSystem);
+        reader = dataReader.newLineNumberReader("testfiles/datafile");
+
+        assertEquals("Data File Contents", reader.readLine());
+    }
+}
